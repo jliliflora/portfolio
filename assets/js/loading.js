eee5571f9b73bc8b5fac9bba2ddf0c976876097e
jliliflora <61509359+jliliflora@users.noreply.github.com>
2022-06-11 15:12:49 +0900

    update 0611 / loading

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,21 +1,20 @@
 // loading
-var percent = document.querySelector('.percent');
-var preload = document.querySelector('.loading');
-var count = 4;
+var percent = document.querySelector(".percent");
+var preload = document.querySelector(".loading");
+var count = 0;
 //var per = 16;
-var loading = setInterval(animate, 1000 / 90);
+var loading = setInterval(animate, 1000 / 70);
 function animate() {
-    if (count >= 100) {
-        clearInterval(loading);
-        preload.classList.add('finished');
-        // loadingAfter();
-        //loadingAfterMain();
-    }
-    else {
-        count = count + 1;
-        percent.textContent = count + '%';
-    }
-    if (count > 99.9) {
-        count = 100;
-    }
+  if (count >= 100) {
+    clearInterval(loading);
+    preload.classList.add("finished");
+    // loadingAfter();
+    //loadingAfterMain();
+  } else {
+    count = count + 1;
+    percent.textContent = count + "%";
+  }
+  if (count > 99.9) {
+    count = 100;
+  }
 }
